d84dc452d8db9314fc62a2ce5719fcf65e09e8a7
moKshagna-p <mokshagnareddy45@gmail.com>
2026-03-24 11:48:56 +0530

    Fix CI by removing unsupported glass APIs

--- Sources/LeetBar/UI/View+PanelSurface.swift
+++ Sources/LeetBar/UI/View+PanelSurface.swift
@@ -1,36 +1,7 @@
 import SwiftUI
 
 extension View {
-    @ViewBuilder
     func panelSurface(cornerRadius: CGFloat, interactive: Bool = false) -> some View {
-        if #available(macOS 26, *) {
-            if interactive {
-                InteractiveGlassSurface(content: self, cornerRadius: cornerRadius)
-            } else {
-                StaticGlassSurface(content: self, cornerRadius: cornerRadius)
-            }
-        } else {
-            self.background(.regularMaterial, in: RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
-        }
-    }
-}
-
-@available(macOS 26, *)
-private struct InteractiveGlassSurface<Content: View>: View {
-    let content: Content
-    let cornerRadius: CGFloat
-
-    var body: some View {
-        content.glassEffect(.regular.interactive(), in: .rect(cornerRadius: cornerRadius))
-    }
-}
-
-@available(macOS 26, *)
-private struct StaticGlassSurface<Content: View>: View {
-    let content: Content
-    let cornerRadius: CGFloat
-
-    var body: some View {
-        content.glassEffect(.regular, in: .rect(cornerRadius: cornerRadius))
+        self.background(.regularMaterial, in: RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
     }
 }
